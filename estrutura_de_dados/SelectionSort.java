7d59c60dcb384759b028fc5dd8412f6b7df79551
brenonun3s <morais.brenonunes@hotmail.com>
2025-09-05 16:00:40 -0300

    feat: desenvolvido os algoritmos SelectionSort e ShellSort

---
+++ estrutura_de_dados/SelectionSort.java
@@ -0,0 +1,44 @@
+package estrutura_de_dados;
+
+public class SelectionSort {
+
+ public static void main(String[] args) {
+  
+  int[] vetor = new int[10];
+
+  System.out.println("Vetor desordenado");
+  for (int i = 0; i < vetor.length; i++) {
+   vetor[i] = (int) (Math.random() * vetor.length);
+   System.out.println( vetor[i]);
+  }
+
+  //SELECTION SORT - COMPLEXIDADE O(N^2)
+
+  int posicao_menor;
+  int aux;
+  for (int i = 0; i < vetor.length; i++) {
+   posicao_menor = i;
+
+   for (int j = i + 1; j < vetor.length; j++) {
+    if(vetor[j] < vetor[posicao_menor]){
+     posicao_menor = j;
+
+    }
+   }
+   aux = vetor[posicao_menor];
+   vetor[posicao_menor] = vetor[i];
+   vetor[i] = aux;
+  }  
+
+  
+
+  System.out.println("Vetor ordenado");
+  for (int i = 0; i < vetor.length; i++) {
+   System.out.println(vetor[i]);
+  }
+
+
+
+ }
+ 
+}
