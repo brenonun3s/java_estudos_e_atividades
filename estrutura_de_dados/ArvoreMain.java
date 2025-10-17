ad120f64200ff230b09597138be8b70f121c75af
brenonun3s <morais.brenonunes@hotmail.com>
2025-10-17 16:59:29 -0300

    feat(Arvore): add codigo referente a Pos Ordem, Pre Ordem e Em Ordem

--- estrutura_de_dados/ArvoreMain.java
+++ estrutura_de_dados/ArvoreMain.java
@@ -3,5 +3,20 @@ public class ArvoreMain {
   Arvore<Integer> arvore = new Arvore<Integer>();
   arvore.adicionar(10);
   arvore.adicionar(5);
+  arvore.adicionar(18);
+  arvore.adicionar(25);
+  arvore.adicionar(11);
+  arvore.adicionar(4);
+  arvore.adicionar(2);
+  arvore.adicionar(3);
+
+  System.out.println("\n\n EM ORDEM: ");
+  arvore.emOrdem(arvore.getRaiz());
+
+  System.out.println("\n\n PRÉ ORDEM: ");
+  arvore.preOrdem(arvore.getRaiz());
+
+  System.out.println("\n\n POS ORDEM: ");
+  arvore.posOrdem(arvore.getRaiz());
  }
 }
