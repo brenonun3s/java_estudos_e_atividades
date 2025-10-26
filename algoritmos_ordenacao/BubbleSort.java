57eca115064fb211e113a0fbb40d78b26bc32c7c
Breno Nunes <morais.brenonunes@hotmail.com>
2025-10-26 19:00:26 -0300

    fix(BubbleSort): corrigido o metodo

--- algoritmos_ordenacao/BubbleSort.java
+++ algoritmos_ordenacao/BubbleSort.java
@@ -19,17 +19,16 @@ public class BubbleSort {
   long inicio = System.currentTimeMillis();
   long fim;
   int aux;
-  for (int i = 0; i < vetor.length; i++) {
-   for (int j = 1 + i; j < vetor.length; j++) {
-    if (vetor[i] > vetor[j]) {
-     aux = vetor[j];
-     vetor[j] = vetor[i];
-     vetor[i] = aux;
-
+  for (int i = 0; i < vetor.length - 1; i++) {
+    for (int j = 0; j < vetor.length - 1 - i; j++) {
+        if (vetor[j] > vetor[j + 1]) {
+            aux = vetor[j];
+            vetor[j] = vetor[j + 1];
+            vetor[j + 1] = aux;
+        }
     }
-   }
+}
 
-  }
   fim =System.currentTimeMillis();
 
   System.out.println("Tempo em ms: " + (fim - inicio));
